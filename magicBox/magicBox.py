53d3286095a3e1204199029534dd9cc03fbd7b38
mi0-1123 <si2.o.mjt@gmail.com>
2018-11-01 18:15:03 +0900

    build object-oriented

--- magicBox/magicBox.py
+++ magicBox/magicBox.py
@@ -1,6 +1,9 @@
-import serial
 import time
 import sys
+
+import serial
+
+from sushi_parameter import SushiParameter
 
 BLUETOOTH = '/dev/ttyAMA0'
 #BLUETOOTH = '/dev/ttyS0'
@@ -11,8 +14,21 @@ if __name__ == '__main__':
     ser = serial.Serial(BLUETOOTH, RATE, timeout=2)
     while True:
         try:
-            strmsg=ser.readline().decode()
-            print('get',strmsg,end='')
+            is_finished = False
+            strmsg=ser.readline().decode()  #受け取ったメッセージをbyte->stringに直してstrmsgに代入してます
+            print('get',strmsg,end='')  #Oya機から受け取った値を出力します
+            received_values = list(strmsg.split(','))
+            sushi_parameter = SushiParameter(received_values[0],received_values[1],received_values[2],received_values[3])
+
+            if sushi_parameter.is_shake and sushi_parameter.is_stick:
+                print('nyan')
+                is_finished = True
+        
+            if is_finished and sushi_parameter.dist < -43.0:
+                print('bow')
+
+
+
         except KeyboardInterrupt:
             ser.close()
             sys.exit(0)
